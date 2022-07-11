62730ac1f649628cf412b56a9f7e961f33275a53
michael <mishkadolgov@mail.ru>
2022-07-11 13:03:59 +0300

    additional functions

--- api.py
+++ api.py
@@ -6,17 +6,17 @@ def collect_stocks_data(token_stocks: str, ticker: str, exchange="NASDAQ", inter
     try:
         result = requests.get(f"https://api.twelvedata.com/time_series?symbol={ticker}:{exchange}"
                               f"&interval={interval}&apikey={token_stocks}")
+        data = result.json()
     except RequestApiError as e:
         print("Something went wrong: ", e)
         return "К сожалению я не могу найти данный актив. "
     print(result.status_code)
     if result.status_code == 200:
-        result.json()
         message_text = "Мета информация:\n"
-        for k, v in result["meta"].items():
+        for k, v in data["meta"].items():
             message_text += k + " : " + v + "\n"
         message_text += "\nДанные актива: \n"
-        for k, v in result["values"][0].items():
+        for k, v in data["values"][0].items():
             message_text += k + " : " + v + "\n"
         return message_text
     else:
@@ -27,5 +27,5 @@ def collect_weather_data(token_weather: str, city: str) -> str:
     return "В разработке ⚒️"
 
 
-def collect_random_news(token_news: str) ->str:
+def collect_random_news(token_news: str) -> str:
     return "В разработке ⚒️"
