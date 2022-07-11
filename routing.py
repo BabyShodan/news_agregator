43abc273d3995a2893f4b4a46e8c467f3a2acb3a
michael <mishkadolgov@mail.ru>
2022-07-11 09:42:20 +0300

    additional functions

--- routing.py
+++ routing.py
@@ -1,4 +1,7 @@
+import os
+
 from aiogram import types
+from aiogram.dispatcher.filters import Text
 
 from main import dp, bot
 import markups as nav
@@ -17,3 +20,13 @@ async def show_list(message: types.Message) -> None:
 async def message_reader(message: types.Message) -> None:
     await bot.send_message(message.from_user.id, "К сожалению я не знаю данной команды 🙁 \n"
                                                  "Для справки введите команду: /help")
+
+
+@dp.message_handler(Text(equals="На главную"))
+async def go_back(message: types.Message) -> None:
+    await bot.send_message(message.from_user.id, "Вы перемещены в главное меню.", reply_markup=nav.MainMenu)
+
+
+@dp.message_handler(Text(equals="Цены активов"))
+async def stocks_cos(message: types.Message) -> None:
+    await bot.send_message(message.from_user.id, "Какие активы вас интересуют?", reply_markup=nav.StocksMenu)
