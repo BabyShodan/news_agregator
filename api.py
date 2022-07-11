6db2e36ec0144fc54243184e15d3eb09439e17c5
michael <mishkadolgov@mail.ru>
2022-07-11 13:11:00 +0300

    additional functions

--- api.py
+++ api.py
@@ -4,7 +4,8 @@ from exceptions import ParseJsonError
 
 def collect_stocks_data(token_stocks: str, ticker: str, exchange="NASDAQ", interval="1min") -> str:
     result = requests.get(f"https://api.twelvedata.com/time_series?symbol={ticker}:{exchange}"
-                          f"&interval={interval}&apikey={token_stocks}")
+                          f"&interval={interval}&apikey={token_stocks}").json()
+    print(result)
     try:
         if result.status_code == 200:
             message_text = "Мета информация:\n"
