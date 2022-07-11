3dce810e58f320729983d4889d6f0ff2a3eae325
michael <mishkadolgov@mail.ru>
2022-07-11 10:38:12 +0300

    additional functions

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ from aiogram.dispatcher.filters import Text
 from aiogram.utils.executor import start_webhook
 from aiogram import Bot, types
 
+import markups as nav
+
 
 BOT_TOKEN = os.getenv("TG_BOT_TOKEN")
 STOCKS_API_KEY = os.getenv("STOCKS_API")
@@ -39,13 +41,13 @@ async def show_list(message: types.Message) -> None:
     await bot.send_message(message.from_user.id,
                            f"Привет {message.from_user.first_name}!\n"
                            "Я бот-агрегатор новостей! 🤖 \n"
-                           "Пожалуйста, выбери функцию из меню")
+                           "Пожалуйста, выбери функцию из меню",
+                           reply_markup=nav.MainMenu)
 
 
 @dp.message_handler()
 async def message_reader(message: types.Message) -> None:
-    await bot.send_message(message.from_user.id, "К сожалению я не знаю данной команды 🙁 \n"
-                                                 "Для справки введите команду: /help")
+    await bot.send_message(message.from_user.id, "К сожалению я не знаю данной команды 🙁 \n")
 
 
 @dp.message_handler(Text(equals="На главную"))
@@ -55,7 +57,7 @@ async def go_back(message: types.Message) -> None:
 
 @dp.message_handler(Text(equals="Цены активов"))
 async def stocks_cos(message: types.Message) -> None:
-    await bot.send_message(message.from_user.id, "Какие активы вас интересуют?")
+    await bot.send_message(message.from_user.id, "Какие активы вас интересуют?", reply_markup=nav.StocksMenu)
 
 
 print(__name__)
