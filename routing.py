c74a037078b7a2ae79e9fa8f4da7733b38a74d1d
michael <mishkadolgov@mail.ru>
2022-07-11 10:27:49 +0300

    additional functions

--- routing.py
+++ routing.py
@@ -16,13 +16,3 @@ async def show_list(message: types.Message) -> None:
 async def message_reader(message: types.Message) -> None:
     await bot.send_message(message.from_user.id, "К сожалению я не знаю данной команды 🙁 \n"
                                                  "Для справки введите команду: /help")
-
-
-@dp.message_handler(Text(equals="На главную"))
-async def go_back(message: types.Message) -> None:
-    await bot.send_message(message.from_user.id, "Вы перемещены в главное меню.")
-
-
-@dp.message_handler(Text(equals="Цены активов"))
-async def stocks_cos(message: types.Message) -> None:
-    await bot.send_message(message.from_user.id, "Какие активы вас интересуют?")
